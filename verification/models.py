b1e183c6361861a216850681edf4491ff8396956
Atheeq <atheeqsa2647@gmail.com>
2023-11-22 19:01:13 +0530

    key will be deleted after 2nd time

--- verification/models.py
+++ verification/models.py
@@ -5,6 +5,7 @@ from django.db import models
 
 class SerialKey(models.Model):
     key = models.CharField(max_length=20, unique=True)
+    used_time = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return self.key
--- verification/views.py
+++ verification/views.py
@@ -1,12 +1,28 @@
 # verification/views.py
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import JsonResponse
 from .models import SerialKey
+
+
+def check_and_update_serial_key(serial_key):
+    key_object = get_object_or_404(SerialKey, key=serial_key, used_time__lt=2)
+
+    # Increment used_time
+    key_object.used_time += 1
+
+    # If used_time is 2 or more, delete the key
+    if key_object.used_time >= 2:
+        key_object.delete()
+    else:
+        key_object.save()
+
+    return key_object.used_time
+
 
 def check_serial_key_in_database(serial_key):
     try:
         key_object = SerialKey.objects.get(key=serial_key)
-        key_object.delete()
+       # key_object.delete()
         return True
     except SerialKey.DoesNotExist:
         return False
@@ -19,6 +35,7 @@ def verify_authenticity(request):
         is_authentic = check_serial_key_in_database(serial_key)
 
         if is_authentic:
+            used_time = check_and_update_serial_key(serial_key)
             response_data = {'result': 'authentic'}
         else:
             response_data = {'result': 'not_authentic'}
