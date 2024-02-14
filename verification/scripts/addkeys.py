01c316790395289f3c92244a2f1c32814ba3fd51
Atheeq1821 <syed21ad067@rmkcet.ac.in>
2024-02-14 20:10:31 +0530

    keys added

--- verification/scripts/addkeys.py
+++ verification/scripts/addkeys.py
@@ -9,7 +9,7 @@ with open(csv_file_path, 'r') as file:
     for row in reader:
         key_value = row['Keys']
         s+=1
-        if s==5:
+        if s==25:
             exit()
         # Create SerialKey object and save to the database
         SerialKey.objects.create(key=key_value, used_time=0)
